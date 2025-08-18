402a86b0370be033247d27836e196cfa229fce45
wuzhaoyang <915030415@qq.com>
2025-08-18 12:34:42 +0800

    header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
-import Header from '@/app/components/Header';
+import Header from '@/components/Header';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -12,7 +12,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
+  return ( 
     <html lang="en">
       <body>
         <Header></Header>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,18 @@
 import Link from 'next/link';
+import { Separator } from '@/components/ui/separator';
 
 export default function Header() {
   return (
-    <div className="h-16 px-0  bg-white">
+    <div className="h-16 px-0 border-b bg-white">
       <div className="container flex items-center justify-between h-full">
         <h1 className="text-2xl">
           <Link href="/">Store</Link>
         </h1>
-        <div className="flex justify-end space-x-4 text-sm">
+        <div className="flex justify-end space-x-4 text-sm h-1/3">
           <Link href="/search">search</Link>
+          <Separator orientation="vertical" />
           <Link href="/account">account</Link>
+          <Separator orientation="vertical" />
           <Link href="/card">card</Link>
         </div>
       </div>
