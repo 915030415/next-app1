068744a4d91dd12d9454982a89b99817d3a0bb4a
wuzhaoyang <915030415@qq.com>
2025-08-19 12:43:44 +0800

    1

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -1,3 +1,14 @@
+import Sort from '@/components/Sort';
+import Products from '@/components/Products';
+import { productsAction } from '@/app/actions/products';
+
 export default async function Home() {
-  return <div>hello page1 </div>;
+  const res = await productsAction();
+  console.log('------log------', res);
+  return (
+    <div className="container flex py-6 ">
+      <Sort></Sort>
+      <Products data={res.data} />
+    </div>
+  );
 }
